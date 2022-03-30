327df2d2e16e9f191e796e4fc8a7b780deaf5263
Minato <ronak@guruz.com>
2022-03-30 18:32:18 +0530

    scsriptable build pipeline integrated

---
+++ YondaimeCDS/Assets/Bundle1/ContentBuilder.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class ContentBuilder : MonoBehaviour
+{
+
+    public const int ChangeLv = 1;
+
+
+}
---
+++ YondaimeCDS/Assets/Bundle2/ContentBuilder1.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class ContentBuilder1 : MonoBehaviour
+{
+
+
+    public const int ChangeLv = 3;
+    
+   
+   
+}
